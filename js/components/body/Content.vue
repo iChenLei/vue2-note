f47a7010441c7c7ec78491a3956786c20ce91bb8
chenlei <2470828450@qq.com>
2016-11-28 23:26:54 +0800

    :smile:add content component

---
+++ js/components/body/Content.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <div class="note-panel">
+        <p>Title:</p>
+        <p>Content</p>
+        <div>
+    </div>
+</template>
+
+<script>
+    export default{
+        //...
+    }
+</script>
+
+<style>
+    .note-panel{
+        backgroud-color:red;
+    }
+</style>
