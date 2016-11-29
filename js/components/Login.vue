ee8f71096d69f56adc748308ca542cb30c9cf0cd
chenlei <2470828450@qq.com>
2016-11-29 23:07:00 +0800

    :rocket:add login component

---
+++ js/components/Login.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <form>
+        <label>Name</label>
+        <input />
+        <label>Password</label>
+        <input />
+        <button>Login In</button>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default{
+        //...
+    }
+</script>
+
+<style>
+    .btn-active{
+        
+    }
+</style>
