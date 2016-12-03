c24ac4e0941027e38101181db4ddebd4e2c5557d
chenlei <2470828450@qq.com>
2016-12-03 22:38:07 +0800

    :rocket:Slide menu

---
+++ js/components/Slide.vue
@@ -0,0 +1,28 @@
+<template>
+      <div id='slide'>
+          <div>
+              <img>Avatar</img>
+              <p>Username</p>
+          </div>
+          <div>
+              <p><a>写日记</a></p>
+              <p><a>看日记</a></p>
+              <p><a>设置</a></p>
+              <p><a>退出</a></p>              
+          </div>
+      </div>  
+</template>
+
+<script>
+    export default{
+        //....
+    }
+</script>
+
+<style>
+    #slide{
+        postition:-100px;
+        width:120px;
+        height:100%;
+    }
+</style>
