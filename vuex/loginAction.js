d080a8c971f18b8bc5b6fed249322a32ee84c480
chenlei <2470828450@qq.com>
2016-12-05 22:40:04 +0800

    What is Vuex?

---
+++ vuex/loginAction.js
@@ -0,0 +1,13 @@
+import {mapActions} from 'vuex';
+
+export default {
+  // ...
+  methods: {
+    ...mapActions([
+      'increment' // 映射 this.increment() 为 this.$store.dispatch('increment')
+    ]),
+    ...mapActions({
+      add: 'increment' // 映射 this.add() 为 this.$store.dispatch('increment')
+    })
+  }
+}
