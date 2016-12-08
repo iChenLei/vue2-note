7ea07df2f44d4da44bde5306f371eb2e79b46ecd
chenlei <2470828450@qq.com>
2016-12-08 23:05:54 +0800

    logo href

--- js/components/head/Logo.vue
+++ js/components/head/Logo.vue
@@ -1,11 +1,17 @@
 <template>
+    <img href="www.baidu.com/img" />
 </template>
 
 <script>
-    
+    export default {
+        
+    }
 </script>
 
 <style>
-
+    img{
+        width:200;
+        height:300;
+    }
 </style>
 
